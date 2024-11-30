d9904988cb905929efd5b8dfb2795edb713d2a00
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2024-11-30 21:07:28 +0530

    Add files via upload
    Prob 1 : Write a program that takes an integer, then a string, then a char from the user and prints them in the screen.
    
    
    Input:  2 Name y
    
    Expected Output:
    
    2
    
    Name
    
    y

---
+++ Week1_Problem1.java
@@ -0,0 +1,28 @@
+
+
+package java_DSA;
+
+import java.util.Scanner;
+
+public class Week1_Problem1 {
+	public static void main(String[] args) {
+		System.out.println("Enter the inputs");		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int num = scan.nextInt();
+		String words = scan.next();
+		char letters = scan.next().charAt(0);
+		
+		
+		System.out.println(num);
+		System.out.println(words);
+		System.out.println(letters);
+		
+	
+			
+	}
+
+	
+
+}
