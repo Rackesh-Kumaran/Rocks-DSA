51f43d923f01fa10d7e1b720cf3b16efab306de9
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2024-11-30 21:16:13 +0530

    Add files via upload
    Prob 2: Write a program to check whether a triangle can be formed with the given values for the angles.
    
    If sum of angles is equal to 180, then triangle can be formed, else it can't be formed.
    
    Input: 45 45 45
    
    Expected Output: 
    
    Triangle cannot be formed
    
    Explanation -> We are getting 3 inputs, that is three angles of triangle, but here the sum of three angles that is 45+45+45 is not equal to 180 so Triangle cannot be formed is the output.

---
+++ Week1_Problem2.java
@@ -0,0 +1,28 @@
+package java_DSA;
+
+import java.util.Scanner;
+
+public class Week1_Problem2 {
+	public static void main(String[] args) {
+		System.out.println("Enter the inputs");
+		
+		Scanner a = new Scanner(System.in);
+		Scanner b = new Scanner(System.in);
+		Scanner c = new Scanner(System.in);
+		
+		int s1 = a.nextInt();
+		int s2 = b.nextInt();
+		int s3 = c.nextInt();
+		
+		int sum = s1+s2+s3;
+		
+		if(sum == 180) {
+			System.out.println("Triangle can be formed");
+		}
+		else {
+			System.out.println("Triangle cannot be formed");
+		}
+	}
+	
+
+}
