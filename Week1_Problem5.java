a767b245afbb6604379a3e6399c3c868261c84ae
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2024-12-01 17:40:26 +0530

    Add files via upload
    Write a program which takes two values x and y. Prints x for y number of times.
    
    Input:
    
    2 
    
    3
    
    Expected Output
    
    2
    
    2
    
    2
    
    Explanation - 2 is x and 3 is y in the input. So 2 is printed 3 times on the output.

---
+++ Week1_Problem5.java
@@ -0,0 +1,23 @@
+package java_DSA;
+import java.util.Scanner;
+
+public class Week1_Problem5 {
+	public static void main(String[] args) {
+		System.out.println("enter a Value");
+		Scanner one = new Scanner(System.in);
+		Scanner two = new Scanner(System.in);
+		
+		int a = one.nextInt();
+		int b = two.nextInt();
+		
+		for(int i=0;i<b;i++) {
+			System.out.println(a);
+			
+		}
+		
+		
+		
+		
+	}
+
+}
