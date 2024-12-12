c111f6880c379f7814f9d1fc1a56db80cb050977
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2024-12-12 19:02:49 +0530

    Add files via upload
    Pattern 12 :
    
    Input:
    
    n = 4
    
    Expected output:
    
       *
      ***
     *****
    *******
     *****
      ***
       *

---
+++ Week2_Pattern12.java
@@ -0,0 +1,34 @@
+package java_DSA;
+
+import java.util.Scanner;
+
+public class Week2_Pattern12 {
+	public static void main (String[] args) {
+		System.out.println("enter the no of stars to print: ");
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		
+		for(int i=1;i<=N-1;i++) {
+			for(int j=1;j<=N-i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+		
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=i-1;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=(2*N)-(2*i-1);j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+	}
+
+}
