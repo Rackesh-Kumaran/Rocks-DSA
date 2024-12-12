eb84d4ad99ffca700668c1b74470979cede17cd7
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2024-12-12 18:42:04 +0530

    Add files via upload
    Pattern4:
    
    Input:
    
    n = 3
    
    Expected output:
    
    *    **      ***

---
+++ Week2_Pattern4.java
@@ -0,0 +1,19 @@
+package java_DSA;
+
+import java.util.Scanner;
+
+public class Week2_Pattern4 {
+	public static void main (String[] args) {
+		System.out.println("enter the no of stars to print: ");
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.print(" ");
+		}
+	}
+
+}
